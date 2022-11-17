81fed84697aeae3228b5a4170f99f9ee72b8e2da
RANGARAAJ VENKKATESAN <63221997+rangaraajv@users.noreply.github.com>
2022-11-17 12:43:05 +0530

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,63 @@
+<template>
+  <div>
+      <div>
+          <p>Marker Size: {{markerSize}}</p>
+          <button @click="setMarkerSize(10)">Reset Marker Size</button>
+          <button @click="setMarkerSize(markerSize - 1)">Decrease Marker Size</button>
+          <button @click="setMarkerSize(markerSize + 1)">Increase Marker Size</button>
+      </div>
+      <ag-charts-vue :options="options"></ag-charts-vue>
+  </div>
+</template>
+<script lang="ts">
+import { defineComponent } from "vue";
+  import {AgChartsVue} from "ag-charts-vue3";
+  export default defineComponent({
+      data() {
+          return {
+              markerSize: 10,
+              options: {}
+          }
+      },
+      components: {
+          AgChartsVue,
+      },
+      methods: {
+          setMarkerSize(markerSize: number) {
+              this.markerSize = markerSize;
+              this.updateOptions();
+          },
+          updateOptions() {
+              this.options = {
+                  data: [{
+                      month: 'Jan',
+                      revenue: 155000,
+                      profit: 33000
+                  }, {
+                      month: 'Feb',
+                      revenue: 123000,
+                      profit: 35500
+                  }, {
+                      month: 'Mar',
+                      revenue: 172500,
+                      profit: 41000
+                  }, {
+                      month: 'Apr',
+                      revenue: 185000,
+                      profit: 50000
+                  }],
+                  series: [{
+                      xKey: 'month',
+                      yKey: 'revenue',
+                      marker: {
+                          size: this.markerSize
+                      }
+                  }]
+              };
+          }
+      },
+      beforeMount() {
+          this.updateOptions();
+      }
+  });
+</script>
